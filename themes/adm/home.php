4be6c41d8a1551e3616ed000e3aae83789405f84
Pilgerr <kauepilger.ch044@academico.ifsul.edu.br>
2022-12-09 20:28:24 -0300

    home admin

--- themes/adm/home.php
+++ themes/adm/home.php
@@ -1,2 +1,18 @@
 <?php $this->layout("_theme"); ?>
-<a href="<?=url("adm/gerar-pdf")?>">Dashboards</a>
+<link rel="stylesheet" href="<?=url("assets/adm/");?>css/home.css">
+
+<div class="header-home">
+<h1>Dashboards</h1>
+<br>
+<p>Acompanhe resultados importantes do dia a dia de seu e-commerce!</p>
+<br>
+<a href="<?=url("adm/gerar-pdf")?>" style="color:rgb(133, 196, 255);text-decoration:underline;">Baixar relatório</a>
+<br><br><hr>
+<br>
+</div>
+        <div class="dashs">
+            <h6>R$ 10</h6>
+            <div class="div-title-dashs">
+                <h6>Teste</h6>
+            </div>
+        </div>
